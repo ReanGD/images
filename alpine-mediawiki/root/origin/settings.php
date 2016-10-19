2eb63d6c7df43a89aa2007a4edc5c929178dffc8
ReanGD <reangd@yandex.ru>
2016-10-20 02:30:27 +0300

    mediawiki settings

---
+++ alpine-mediawiki/root/origin/settings.php
@@ -0,0 +1,90 @@
+<?php
+
+## Shared memory settings
+$wgMainCacheType = CACHE_MEMCACHED;
+$wgMemCachedServers = [ '127.0.0.1:11211' ];
+
+## To enable image uploads, make sure the 'images' directory
+## is writable, then set this to true:
+$wgEnableUploads = true;
+#$wgUseImageMagick = true;
+#$wgImageMagickConvertCommand = "/usr/bin/convert";
+
+# The following permissions were set based on your choice in the installer
+$wgGroupPermissions['*']['createaccount'] = false;
+if ( $_SERVER['REMOTE_ADDR'] == '127.0.0.1' ) {
+ 	$wgGroupPermissions['*']['read'] = true;
+ 	$wgGroupPermissions['*']['edit'] = true;
+}else{
+	$wgGroupPermissions['*']['edit'] = false;
+	$wgGroupPermissions['*']['read'] = false;
+}
+
+# Cache
+// Отключение показов страниц счетчики.
+$wgDisableCounters = true;
+
+// $wgMainCacheType = CACHE_ACCEL;
+// $wgMessageCacheType = CACHE_ACCEL;
+// $wgCacheDirectory = '<SOME DIRECTORY>';
+// $wgUseLocalMessageCache = true;
+// $wgParserCacheType = CACHE_ACCEL;
+// $wgMemCachedServers = array();
+// $wgUseGzip = true;
+// $wgEnableSidebarCache = true;
+
+// # NO DB HITS!
+// $wgMiserMode = true;
+
+// # Text cache
+// $wgCompressRevisions = true; // use with care (see talk page)
+// $wgRevisionCacheExpiry = 3*24*3600;
+// $wgParserCacheExpireTime = 14*24*3600;
+
+// # Diffs (defaults seem ok for Ubuntu and others)
+// $wgDiff = 'C:/Server/xampp/htdocs/MW/bin/GnuWin32/bin/diff.exe';
+// $wgDiff3 = 'C:/Server/xampp/htdocs/MW/bin/GnuWin32/bin/diff3.exe';
+
+# Extensions
+
+############## WikiEditor ##############
+wfLoadExtension( 'WikiEditor' );
+# Расширение включено по умолчанию, однако пользователь может отключить его в настройках.
+$wgDefaultUserOptions['usebetatoolbar'] = 1;
+# Enables link and table wizards by default but still allows users to disable them in preferences
+$wgDefaultUserOptions['usebetatoolbar-cgd'] = 1;
+# Показывает вкладки «Предпросмотра» и «Изменений»
+$wgDefaultUserOptions['wikieditor-preview'] = 1;
+# Показывает кнопки «Публикации» и «Отмены» справа сверху
+$wgDefaultUserOptions['wikieditor-publish'] = 1;
+
+
+############## VisualEditor ##############
+wfLoadExtension( 'VisualEditor' );
+// Расширение включено по умолчанию для всех
+$wgDefaultUserOptions['visualeditor-enable'] = 1;
+// Показывать окно приветствия
+$wgVisualEditorShowBetaWelcome = false;
+
+// Optional: Set VisualEditor as the default for anonymous users
+// otherwise they will have to switch to VE
+// $wgDefaultUserOptions['visualeditor-editor'] = "visualeditor";
+
+// Don't allow users to disable it
+// $wgHiddenPrefs[] = 'visualeditor-enable';
+
+// OPTIONAL: Enable VisualEditor's experimental code features
+// $wgDefaultUserOptions['visualeditor-enable-experimental'] = 1;
+
+$wgVirtualRestConfig['modules']['parsoid'] = array(
+        // URL to the Parsoid instance
+        'url' => 'http://127.0.0.1:8000',
+        // Parsoid "domain" (optional)
+        'domain' => '127.0.0.1'
+        // Parsoid "prefix" (optional)
+        // 'prefix' => 'wiki'
+);
+
+############## Other ##############
+wfLoadExtension( 'InputBox' );
+wfLoadExtension( 'SyntaxHighlight_GeSHi' );
